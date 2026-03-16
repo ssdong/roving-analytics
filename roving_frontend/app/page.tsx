7e3a8566af5fc417fe6006b258fef967768b936e
susu dong <susudong5@gmail.com>
2026-03-15 21:53:14 -0400

    fix: trivial dashboard fix [ROVI-2]

--- roving_frontend/app/page.tsx
+++ roving_frontend/app/page.tsx
@@ -1,28 +1,24 @@
+'use client';
+
 import { Card, Title, Text } from '@tremor/react';
-import { queryBuilder } from '../lib/planetscale';
 import Search from './search';
 import UsersTable from './table';
 
 export const dynamic = 'force-dynamic';
 
-export default async function IndexPage({
+export default function IndexPage({
   searchParams
 }: {
   searchParams: { q: string };
 }) {
-  const search = searchParams.q ?? '';
-  // const users = await queryBuilder
-  //   .selectFrom('users')
-  //   .select(['id', 'name', 'username', 'email'])
-  //   .where('name', 'like', `%${search}%`)
-  //   .execute();
-
-  const users = [{
+  const users = [
+    {
       id: 1,
-      name: "Justin",
-      username: "justinsdong",
-      email: "test@gmail.com"
-  }];
+      name: 'Justin',
+      username: 'justinsdong',
+      email: 'test@gmail.com'
+    }
+  ];
 
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
